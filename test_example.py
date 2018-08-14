028b22e3e419febb6b8e35155384137801b629c5
NinaFilatova <nina.filatova.a1@gmail.com>
2018-08-14 21:21:56 +0300

    Задание 1. Подготовьте инфраструктуру
    подключите к проекту Selenium и сделайте небольшой работающий тест

--- test_example.py
+++ test_example.py
@@ -1,20 +1,23 @@
-import time
 import pytest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-@pytest.fixture
+@pytest.fixture()
 def driver(request):
     wd = webdriver.Chrome()
-    request.addfinalizer(wd.quit)
+
+    def fin():
+        wd.quit()
+    request.addfinalizer(fin)
     return wd
 
 
 def test_example(driver):
-    driver.get("http://www.google.com/")
-    driver.find_element_by_name("q").send_keys("webdriver")
-    driver.find_element_by_name("btnG").click()
+    driver.get("http://www.google.ru/")
+    elem = driver.find_element_by_name("q")
+    elem.send_keys("webdriver")
+    elem.send_keys(Keys.RETURN)
     WebDriverWait(driver, 10).until(EC.title_is("webdriver - Поиск в Google"))
-    time.sleep(5)
