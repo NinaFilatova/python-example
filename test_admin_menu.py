51127b16fdc0540ef1c36a96439d6a51731def02
NinaFilatova <nina.filatova.a1@gmail.com>
2018-08-22 13:35:49 +0300

    Задание №7 Сделайте сценарий, проходящий по всем разделам админки
    делайте сценарий, который выполняет следующие действия в учебном приложении litecart.
    
    1) входит в панель администратора http://localhost/litecart/admin
    2) прокликивает последовательно все пункты меню слева, включая вложенные пункты
    3) для каждой страницы проверяет наличие заголовка (то есть элемента с тегом h1)
    
    Можно оформить сценарий либо как тест, либо как отдельный исполняемый файл.

---
+++ test_admin_menu.py
@@ -0,0 +1,34 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    wd.implicitly_wait(1)
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_menu(driver):
+    driver.get("http://localhost/litecart/admin/")
+    driver.find_element_by_name("username").send_keys("admin")
+    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_name("remember_me").click()
+    driver.find_element_by_name("login").click()
+
+    menu = driver.find_elements_by_xpath("//ul[@id='box-apps-menu']//li[@id='app-']")
+    for i in range(len(menu)):
+        menu[i].click()
+        h1 = driver.find_elements_by_xpath("//h1")
+        if len(h1) == 0:
+            print("Page hasn't elements h1")
+        second = driver.find_elements_by_xpath("//li[@id='app-' and contains(@class, 'selected')]//li")
+        if len(second) > 0:
+            for k in range(len(second)):
+                second[k].click()
+                h1 = driver.find_elements_by_xpath("//h1")
+                if len(h1) == 0:
+                    print("Page hasn't elements h1")
+                second = driver.find_elements_by_xpath("//li[@id='app-' and contains(@class, 'selected')]//li")
+        menu = driver.find_elements_by_xpath("//ul[@id='box-apps-menu']//li[@id='app-']")
